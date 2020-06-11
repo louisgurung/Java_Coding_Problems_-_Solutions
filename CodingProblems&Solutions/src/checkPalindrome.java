9325f4cbdd35059496a7470a1ba5e7348f015efe
Louis Gurung <louis.gurung@selu.edu>
2020-06-11 11:09:19 -0500

    First Commit

---
+++ CodingProblems&Solutions/src/checkPalindrome.java
@@ -0,0 +1,8 @@
+//Program to say whether the input is a palindrome or not:: 
+//Test Input::: 
+//Ouput:::
+public class checkPalindrome {
+	public static void main(String[] args) {
+		System.out.print("Commit Check");
+	}
+}
